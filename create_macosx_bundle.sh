a756b3b7ccf2713c07c9cc905b486d2027e99f8b
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-01-10 02:39:17 +0000

    Scripts to create Mac OS X bundle.
    Generic in scripts/, based on Lazarus examples/trayicon/.
    
    And specific in view3dscene, using the generic one.
    
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/scripts@7687 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ create_macosx_bundle.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -eu
+
+# Create a xxx.app directory being a Mac OS X "bundle" (magic directory
+# that behaves like a clickable application under Mac OS X).
+# This is based on createbundle.sh script from Lazarus examples/trayicon/,
+# improved and generalized by Kambi.
+#
+# $1 is the nice application name and folder basename. This is traditionally
+# in CamelCase.
+#
+# $2 is the binary name, must be in current dir.
+#
+# $3 is the icon filename, relative to current dir.
+
+appname="$1"
+appfolder=$appname.app
+macosfolder=$appfolder/Contents/MacOS
+plistfile=$appfolder/Contents/Info.plist
+appfile="$2"
+
+if ! [ -e $appfile ]; then
+  echo "$appfile binary does not exist."
+  echo "Run something like  ./compile.sh or ./compile_unix.sh or make build-unix first"
+  exit 1
+fi
+
+if [ -e $appfolder ]; then
+  echo "$appfolder already exists, removing"
+  rm -Rf $appfolder
+fi
+
+echo "Creating $appfolder..."
+mkdir $appfolder
+mkdir $appfolder/Contents
+mkdir $appfolder/Contents/MacOS
+mkdir $appfolder/Contents/Resources
+
+cp $appfile $macosfolder/$appname
+
+# Copy the resource files to the correct place
+cp "$3" $appfolder/Contents/Resources
+
+# Create PkgInfo file.
+echo "APPL????" >$appfolder/Contents/PkgInfo
+
+# Create information property list file (Info.plist).
+echo '<?xml version="1.0" encoding="UTF-8"?>' >$plistfile
+echo '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >>$plistfile
+echo '<plist version="1.0">' >>$plistfile
+echo '<dict>' >>$plistfile
+echo '  <key>CFBundleDevelopmentRegion</key>' >>$plistfile
+echo '  <string>English</string>' >>$plistfile
+echo '  <key>CFBundleExecutable</key>' >>$plistfile
+echo '  <string>'$appname'</string>' >>$plistfile
+echo '  <key>CFBundleInfoDictionaryVersion</key>' >>$plistfile
+echo '  <string>6.0</string>' >>$plistfile
+echo '  <key>CFBundlePackageType</key>' >>$plistfile
+echo '  <string>APPL</string>' >>$plistfile
+echo '  <key>CFBundleSignature</key>' >>$plistfile
+echo '  <string>????</string>' >>$plistfile
+echo '  <key>CFBundleVersion</key>' >>$plistfile
+echo '  <string>1.0</string>' >>$plistfile
+echo '  <key>CSResourcesFileMapped</key>' >>$plistfile
+echo '  <true/>' >>$plistfile
+echo '</dict>' >>$plistfile
+echo '</plist>' >>$plistfile
+
+echo "Done."
