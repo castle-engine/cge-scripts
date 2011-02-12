777d95ef6cbd64add88fcf318f09d03c6a6a2b40
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-02-12 11:25:11 +0000

    Better update_copyright_year.sh: only visit Pascal, txt files.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/scripts@8219 afe8dcc5-9094-451b-8773-5f495042a737

--- update_copyright_year.sh
+++ update_copyright_year.sh
@@ -2,14 +2,14 @@
 set -eu
 
 cd ../
-
-make -C kambi_vrml_game_engine/ cleanmore
-
-# dircleaner from http://michalis.ii.uni.wroc.pl/wsvn/michalis/dircleaner/
-dircleaner . clean
-
-find kambi_vrml_game_engine/src/ '(' -type d -name .svn -prune ')' -or \
-       '(' -type d -name .git -prune ')' -or \
-       '(' -type f -exec emacs --batch \
-         -l scripts/update_copyright_year.el '{}' \
-         -f kam-update-copyright-year ';' ')'
+find view3dscene/ castle/ kambi_vrml_game_engine/ \
+  '(' -type d -name .svn -prune ')' -or \
+  '(' -type d -name .git -prune ')' -or \
+  '(' -type f \
+    '(' -iname '*.pas' -or \
+        -iname '*.inc' -or \
+        -iname '*.txt' -or \
+        -iname '*.lpr' -')' \
+    -exec emacs --batch \
+    -l scripts/update_copyright_year.el '{}' \
+    -f kam-update-copyright-year ';' ')'
