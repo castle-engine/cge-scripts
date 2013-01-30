4929ee6d65ab0f393af41890debb50aa9d33ff85
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-01-30 06:26:47 +0000

    Update copyrights to 2013.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/scripts@12427 afe8dcc5-9094-451b-8773-5f495042a737

--- update_copyright_year.el
+++ update_copyright_year.el
@@ -1,4 +1,4 @@
-;; Copyright 2006-2012 Michalis Kamburelis.
+;; Copyright 2006-2013 Michalis Kamburelis.
 ;;
 ;; This file is part of "Castle Game Engine".
 ;;
@@ -55,6 +55,6 @@ Uses regexps. FIXEDCASE has the same meaning as in `replace-match'."
   ;; append year to my copyrights
   (kam-simple-re-replace-buffer
     "Copyright \\([0-9][0-9][0-9][0-9]\\)\\([,-][-0-9,]+\\)? Michalis Kamburelis."
-    "Copyright \\1-2012 Michalis Kamburelis.")
+    "Copyright \\1-2013 Michalis Kamburelis.")
   (save-buffer)
 )
--- update_copyright_year.sh
+++ update_copyright_year.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu
 
+# Update copyright year in all the sources.
+# Edit update_copyright_year.el first, to set last year
+# in kam-update-copyright-year.
+#
+# Note: you should also update by hand ../vrml_engine_doc/vrml_engine.xml
+
 find ../ \
   '(' -type d -name .svn -prune ')' -or \
   '(' -type d -name .git -prune ')' -or \
