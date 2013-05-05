21d7b10176cab74bab8958ab20bdee5a06e7bbf8
Michalis Kamburelis <michalis.kambi@gmail.com>
2013-05-05 02:59:03 +0000

    Add SVN mime-type for all .x3d, .x3dv, .wrl files. This makes browsing SVN repo through http: download files with correct MimeType.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/scripts@12737 afe8dcc5-9094-451b-8773-5f495042a737

--- svn_set_common_props.sh
+++ svn_set_common_props.sh
@@ -4,11 +4,15 @@ set -eu
 #DEBUG='echo'
 DEBUG=''
 
-#find ../ -iname *.png -exec $DEBUG svn propset svn:mime-type image/png '{}' ';'
-#find ../ -iname *.jpg -exec $DEBUG svn propset svn:mime-type image/jpeg '{}' ';'
+# find ../ -iname *.png -exec $DEBUG svn propset svn:mime-type image/png '{}' ';'
+# find ../ -iname *.jpg -exec $DEBUG svn propset svn:mime-type image/jpeg '{}' ';'
 
-find ../ '(' -iname *.pas -or \
-             -iname *.inc -or \
-             -iname *.lpr \
-         ')' -exec $DEBUG svn propset svn:eol-style native '{}' ';' \
-             -exec $DEBUG svn propset svn:mime-type text/pascal '{}' ';'
+# find ../ '(' -iname *.pas -or \
+#              -iname *.inc -or \
+#              -iname *.lpr \
+#          ')' -exec $DEBUG svn propset svn:eol-style native '{}' ';' \
+#              -exec $DEBUG svn propset svn:mime-type text/pascal '{}' ';'
+
+# find ../ -iname *.x3d -exec $DEBUG svn propset svn:mime-type model/x3d+xml '{}' ';'
+# find ../ -iname *.x3dv -exec $DEBUG svn propset svn:mime-type model/x3d+vrml '{}' ';'
+# find ../ -iname *.wrl -exec $DEBUG svn propset svn:mime-type model/vrml '{}' ';'
